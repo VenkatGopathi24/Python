18cc3e0c8d4c996214cf3e9e983ac1026a309e6d
VenkatGopathi24 <147724948+VenkatGopathi24@users.noreply.github.com>
2023-10-17 12:14:20 +0530

    Add files via upload

---
+++ enumerate.assigment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a = ["apple","bat","gopathi","venkata","naveen"]
+my_dict = {}
+for index,i in enumerate(a):
+    my_dict[index]= i
+print(my_dict)
+
+
+# In[ ]:
+
+
+
+
